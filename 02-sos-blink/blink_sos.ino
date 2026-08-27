2228c3473792dc6e708153fc1f7c96840e268690
Ramanpreetk19 <83728165+Ramanpreetk19@users.noreply.github.com>
2026-08-27 17:41:32 +0200

    Add blink_sos code

---
+++ 02-sos-blink/blink_sos.ino
@@ -0,0 +1,56 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2,OUTPUT);
+
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(800);
+  
+  digitalWrite(2,HIGH);
+  delay(100);
+  digitalWrite(2,LOW);
+  delay(100);
+
+  digitalWrite(2,HIGH);
+  delay(100);
+  digitalWrite(2,LOW);
+  delay(100);
+
+  digitalWrite(2,HIGH);
+  delay(100);
+  digitalWrite(2,LOW);
+  delay(100);
+
+  digitalWrite(2,HIGH);
+  delay(500);
+  digitalWrite(2,LOW);
+  delay(500);
+
+  digitalWrite(2,HIGH);
+  delay(500);
+  digitalWrite(2,LOW);
+  delay(500);
+
+  digitalWrite(2,HIGH);
+  delay(500);
+  digitalWrite(2,LOW);
+  delay(500);
+
+  digitalWrite(2,HIGH);
+  delay(100);
+  digitalWrite(2,LOW);
+  delay(100);
+
+  digitalWrite(2,HIGH);
+  delay(100);
+  digitalWrite(2,LOW);
+  delay(100);
+
+  digitalWrite(2,HIGH);
+  delay(100);
+  digitalWrite(2,LOW);
+  delay(100);
+
+  delay(1000);
+}
